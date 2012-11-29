1a071d8c921ee4fa71f0ed16c5e7e19862f8d62f
Thomas RICHARD <thomas.richard.pro@gmail.com>
2012-11-29 16:25:01 +0100

    Updating map drawing.

--- pacman/pacman/Map.cs
+++ pacman/pacman/Map.cs
@@ -9,30 +9,31 @@ using Microsoft.Xna.Framework.Content;
 
 namespace pacman
 {
-    public class Map : GameElement
-    {
-        private byte[,] _map;
-        private Texture2D _tiles;
+	public class Map : GameElement
+	{
+		private byte[,] _map;
+		private Texture2D _tiles;
 
-        public Map() : base(new Vector2(16, 16))
-        {
-            Initialize();
-        }
+		public Map()
+			: base(new Vector2(16, 16))
+		{
+			Initialize();
+		}
 
-        public bool isWall(Vector2 coordinates)
-        {
-            return _map[(int)coordinates.Y, (int)coordinates.X] == 0;
-        }
+		public bool isWall(Vector2 coordinates)
+		{
+			return _map[(int)coordinates.Y, (int)coordinates.X] <= 11;
+		}
 
-        private Vector2 WinToMap(Vector2 coordinates)
-        {
-            return coordinates / _spriteSize;
-        }
+		public Vector2 WinToMap(Vector2 coordinates)
+		{
+			return coordinates / _spriteSize;
+		}
 
-        //TODO
-        public override void Initialize()
-        {
-            _map = _map = new byte[31, 28] {
+		//TODO
+		public override void Initialize()
+		{
+			_map = _map = new byte[31, 28] {
                 { 8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9},
                 { 3, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,  2,  3, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,  2},
                 { 3, 12,  4,  1,  1,  5, 12,  4,  1,  1,  1,  5, 12,  2,  3, 12,  4,  1,  1,  1,  5, 12,  4,  1,  1,  5, 12,  2},
@@ -65,44 +66,44 @@ namespace pacman
                 { 3, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,  2},
                 {10,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 11}
             };
-        }
-        //TODO
-        public override void LoadContent(ContentManager content)
-        {
-            _texture = content.Load<Texture2D>("mapTexture");
-        }
-        //TODO
-        public override void Update(GameTime gameTime)
-        {
-            return;
-        }
+		}
+		//TODO
+		public override void LoadContent(ContentManager content)
+		{
+			_texture = content.Load<Texture2D>("mapTexture");
+		}
+		//TODO
+		public override void Update(GameTime gameTime)
+		{
+			return;
+		}
 
-        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            for (int y = 0; y < _map.GetLength(0); ++y)
-            {
-                for (int x = 0; x < _map.GetLength(1); ++x)
-                {
-                    
-                    if (_map[y, x] < 14)
-                    {
-                        spriteBatch.Draw(_texture, new Vector2(x, y) * _spriteSize, new Rectangle( (int)(_spriteSize.X * _map[y, x]), 0, (int)_spriteSize.X, (int)_spriteSize.Y), Color.White);
-                    }
-                }
-            }
-        }
-        /*
-        //TODO
-        public override void UnloadContent(ContentManager content)
-        {
-            return;
-        }
-        //*/
+		public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+		{
+			for (int y = 0; y < _map.GetLength(0); ++y)
+			{
+				for (int x = 0; x < _map.GetLength(1); ++x)
+				{
 
-        public Texture2D Tiles
-        {
-            get { return _tiles; }
-            set { _tiles = value; }
-        }
-    }
+					if (_map[y, x] < 14)
+					{
+						Vector2 pos = new Vector2(x, y) * _spriteSize;
+						Rectangle clipping = new Rectangle(
+							(int)(_spriteSize.X * _map[y, x]),
+							0,
+							(int)_spriteSize.X,
+							(int)_spriteSize.Y);
+
+						spriteBatch.Draw(_texture, pos, clipping, Color.White);
+					}
+				}
+			}
+		}
+
+		public Texture2D Tiles
+		{
+			get { return _tiles; }
+			set { _tiles = value; }
+		}
+	}
 }
